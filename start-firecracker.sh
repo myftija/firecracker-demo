bd1193df6c44a04962abff1f0d029bd1ba31176f
saadi <saadi.myftija@gmail.com>
2025-06-25 15:13:56 +0200

    Add network readniess checks on tcp port 22 after vm booting

--- start-firecracker.sh
+++ start-firecracker.sh
@@ -151,4 +151,26 @@ EOF
   fi
 } &
 
+# non-blocking network readiness check - TCP port 22
+{
+  max_attempts=600
+  attempt=0
+
+  while [ $attempt -lt $max_attempts ]; do
+    if nc -z -w 1 "${FC_IP}" 22 >/dev/null 2>&1; then
+      end_time=$(date +%s.%N)
+      time_diff_sec=$(echo "$end_time - $boot_call_ts" | bc -l)
+      time_diff_ms=$(printf "%.0f" $(echo "$time_diff_sec * 1000" | bc -l))
+      echo "BOOT_TO_NETWORK_READY_TCP22_MS ${time_diff_ms}" >> "${logfile}"
+      break
+    fi
+    attempt=$((attempt + 1))
+    sleep 0.05
+  done
+
+  if [ $attempt -ge $max_attempts ]; then
+    echo "BOOT_TO_NETWORK_READY_TCP22_TIMEOUT" >> "${logfile}"
+  fi
+} &
+
 
